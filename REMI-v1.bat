0f9382486e231cb59bf56062e5a8638eb650b66e
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2025-05-06 08:33:35 +0000

    Added .bat file to run it

---
+++ REMI-v1.bat
@@ -0,0 +1,7 @@
+@echo off
+echo ----------------- Checking for Updates ----------------
+git pull origin main
+echo --------------------- Starting Up ---------------------
+python3.11.exe -u ./v1/main.py
+cls
+call REMI-v1.bat

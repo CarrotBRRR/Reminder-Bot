b0f22f71776f72c11e115cecb324cb59165bfaff
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2025-06-09 23:00:59 -0600

    Update to python 3.13

--- REMI-v1.bat
+++ REMI-v1.bat
@@ -2,6 +2,6 @@
 echo ----------------- Checking for Updates ----------------
 git pull origin main
 echo --------------------- Starting Up ---------------------
-python3.11.exe -u ./v1/main.py
+python3.13.exe -u ./v1/main.py
 cls
 call REMI-v1.bat

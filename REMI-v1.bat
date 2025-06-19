97e4a207b17cbb5d39a9a51672801243621e2ec4
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2025-06-19 15:56:56 -0600

    Cleaned up folder structure

--- REMI-v1.bat
+++ REMI-v1.bat
@@ -3,6 +3,6 @@ cls
 echo ----------------- Checking for Updates ----------------
 git pull origin main
 echo --------------------- Starting Up ---------------------
-python3.13.exe -u ./v1/main.py
+python3.13.exe -u ./v1/Remi-1.0.0.py
 cls
 call REMI-v1.bat
